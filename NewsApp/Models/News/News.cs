a6fd69938b486bf6e0ed1fe3a8dbf285e29cd18e
kairias97 <kevin.irias@inaxir.com>
2018-12-10 11:55:31 -0600

    Added model for News entity.

---
+++ NewsApp/Models/News/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+    }
+}
