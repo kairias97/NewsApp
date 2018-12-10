547be0fa3fc0057f0d5a50bd294d0bcda3314e86
kairias97 <kevin.irias@inaxir.com>
2018-12-10 11:56:28 -0600

    Created DbContext configuration for the NewsApp using FluentAPI

---
+++ NewsApp/Models/NewsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp.Models
+{
+    public class NewsDbContext: DbContext
+    {
+        public DbSet<News> News { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<News>()
+                .Property(n => n.Title)
+                .IsRequired(true);
+
+            modelBuilder.Entity<News>()
+                .Property(n => n.Content)
+                .IsRequired(true);
+
+            modelBuilder.Entity<News>()
+                .Property(n => n.ImageURL)
+                .IsRequired(true);
+
+            modelBuilder.Entity<News>()
+                .Property(n => n.PublishedDate)
+                .IsRequired(true);
+
+        }
+    }
+}
