d0bf7506cc00e4b4b5d816c471aa22826cba9c43
kairias97 <kevin.irias@inaxir.com>
2018-12-10 19:03:45 -0600

    Added Home controller with functionality to fetch featured news

---
+++ NewsApp.Client/app/app.module.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+const apiHost = 'http://localhost:47441/';
+var app = angular.module('newsApp', [
+    'ngRoute'
+]);
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('');
+}]);
+
+app.controller('HomeController', function ($scope, $http) {
+    $scope.news = [];
+    $scope.error = false;
+    
+    function getFeaturedNews() {
+        
+        $http.get(`${apiHost}api/news/featured`).then(function (response) {
+            $scope.news = response.data;
+
+            $scope.fetched = true;
+        }).catch(function (error) {
+            $scope.news = [];
+            $scope.error = true;
+        })
+    }
+    getFeaturedNews();
+});
+
+
+
