b3d007773566b07de3e5b5fa9c728183eabbd2b2
kairias97 <kevin.irias@inaxir.com>
2018-12-10 20:31:00 -0600

    Served the static files through the node js server.

--- NewsApp.Client/server.js
+++ NewsApp.Client/server.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 1337;
 const express = require('express');
 const app = express();
 
-app.get('/*', function (req, res) {
+app.get('/app/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app', req.params[0]));
+})
+    .get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public','index.html'));
 }).listen(port, function () {
     console.log(`Listening in port: ${port}`);
