e2aa9cab89f3a3f29ccc95748aeda948b708e9d3
kairias97 <kevin.irias@inaxir.com>
2018-12-10 20:25:30 -0600

    Revert "Added navbar and imports for angular services of ngSanitize and ngRoute."
    This reverts commit c1037296bcc208cdeb223c2af16a7b0969287af0.

--- NewsApp.Client/server.js
+++ NewsApp.Client/server.js
@@ -4,18 +4,8 @@ const port = process.env.PORT || 1337;
 const express = require('express');
 const app = express();
 
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/app/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'app', req.params[0]));
-}).get('/*', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public','index.html'));
-})
-    .listen(port, function () {
+}).listen(port, function () {
     console.log(`Listening in port: ${port}`);
 });
